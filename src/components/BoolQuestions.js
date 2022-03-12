654093555ea15b57d5967d35da9a3a4100d2b6f3
lorismilloni <lorismsimon@protonmail.comr>
2022-03-11 21:33:32 -0300

    arruma erros de lint e inicia uma nova lógica para a 5

--- src/components/BoolQuestions.js
+++ src/components/BoolQuestions.js
@@ -1,46 +1,88 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+
+// minha ideia aqui foi fazer uma função que faz as comparações e chamá-la dentro de uma section
 
 class BoolQuestions extends React.Component {
   render() {
     const {
       category,
       question,
-      difficulty,
       correctAnswer,
       incorrectAnswers,
     } = this.props;
+
+    const answers = incorrectAnswers;
+    const index = answers.indexOf(`${incorrectAnswers}`);
+
+    isCorrect = () => {
+      if (correctAnswer === 'True') {
+        return (
+          <>
+            <h2
+              data-testid="question-category"
+            >
+              {' '}
+              {category}
+            </h2>
+            <h3
+              data-testid="question-text"
+            >
+              {question}
+            </h3>
+            <div data-testid="answer-options">
+              <button type="button" data-testid="correct-answer">Verdadeiro</button>
+              <button
+                type="button"
+                data-testid={ `wrong-answer-${index}` }
+              >
+                Falso
+              </button>
+            </div>
+          </>
+        );
+      }
+      if (correctAnswer === 'False') {
+        return (
+          <>
+            <h2
+              data-testid="question-category"
+            >
+              {' '}
+              {category}
+            </h2>
+            <h3
+              data-testid="question-text"
+            >
+              {question}
+            </h3>
+            <div data-testid="answer-options">
+              <button type="button" data-testid="correct-answer">Falso</button>
+              <button
+                type="button"
+                data-testid={ `wrong-answer-${index}` }
+              >
+                Verdadeiro
+              </button>
+            </div>
+          </>
+        );
+      }
+    };
+
     return (
-      <>
-        <h2
-          data-testid="question-category"
-        >
-          {' '}
-          { category }
-        </h2>
-
-        <h3
-          data-testid="question-text"
-        >
-          { question }
-        </h3>
-
-        { correct_answer
-          ? <div data-testid="answer-options">
-            <button type="button" data-testid="correct-answer">True</button>
-          </div>
-          : <div data-testid="answer-options">
-            <button type="button" data-testid="correct-answer">False</button>
-          </div> }
-      </>
-
+      <section>
+        { this.isCorrect }
+      </section>
     );
   }
 }
 
 BoolQuestions.propTypes = {
-  dispatch: PropTypes.func,
+  category: PropTypes.string,
+  question: PropTypes.string,
+  correctAnswer: PropTypes.string,
+  incorrectAnswer: PropTypes.string,
 }.isRequired;
 
-export default connect()(BoolQuestions);
+export default BoolQuestions;
--- src/components/Questions.js
+++ src/components/Questions.js
@@ -3,9 +3,23 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { questions } from '../redux/actions/questions';
 import { getQuestionsFromAPI } from '../APIservices/Api';
+import BoolQuestions from './BoolQuestions';
 import MultQuestions from './MultQuestions';
 
+// a ideia aqui era fazer as comparações numa função e chamá-la, mas não funcionou
+
 class Questions extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      category,
+      question,
+      type,
+      correctAnswer,
+      incorrectAnswers,
+    };
+  }
+
   componentDidMount() {
     this.gettingQuestions();
   }
@@ -18,25 +32,32 @@ class Questions extends React.Component {
     dispatch(questions(results));
   }
 
-  render() {
-    const {
-      category,
-      question,
-      type,
-      difficulty,
-      correctAnswer,
-      incorrectAnswers,
-    } = this.props;
+  boolOrMultiple = () => {
+    if (type === 'multiple') {
+      return (
+        <MultQuestions
+          category={ category }
+          question={ question }
+          correctAnswer={ correctAnswer }
+          incorrectAnswers={ incorrectAnswers }
+        />
+      );
+    }
+    if (type === 'boolean') {
+      return (
+        <BoolQuestions
+          category={ category }
+          question={ question }
+          correctAnswer={ correctAnswer }
+          incorrectAnswers={ incorrectAnswers }
+        />
+      );
+    }
+  };
 
+  render() {
     return (
-      <MultQuestions
-        type={ type }
-        category={ category }
-        question={ question }
-        difficulty={ difficulty }
-        correct_answer={ correctAnswer }
-        incorrect_answers={ incorrectAnswers }
-      />
+      { this.boolOrMultiple }
     );
   }
 }
@@ -44,6 +65,10 @@ class Questions extends React.Component {
 Questions.propTypes = {
   dispatch: PropTypes.func,
   token: PropTypes.string,
+  category: PropTypes.string,
+  question: PropTypes.string,
+  correctAnswer: PropTypes.string,
+  incorrectAnswer: PropTypes.string,
 }.isRequired;
 
 const mapStateToProps = ({ token, questions }) => ({
